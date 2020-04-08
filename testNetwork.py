35b756029508f15797c8e83c77cabb463deb08a4
unknown <keonsanavandijoon@gmail.com>
2020-04-08 18:59:48 -0400

    Setup 1 hot better and added more hidden layers

--- testNetwork.py
+++ testNetwork.py
@@ -7,8 +7,17 @@ import datetime
 import torch
 import random
 
+def make1HotVector(labels, value):
+    outArray = []
+    for x in labels:
+        if value == x:
+            outArray.append(1)
+        else:
+            outArray.append(0)
+    return outArray
+
 class Listing:
-    def __init__(self, borough, roomType, reviews, latestReview, reviewsPerMonth, numListings, numDays, price):
+    def __init__(self, borough, neighborhood, roomType, reviews, latestReview, reviewsPerMonth, numListings, numDays, price):
         self.reviews = float(reviews)
         if (self.reviews == 0):
             self.reviewsPerMonth = 0
@@ -17,15 +26,11 @@ class Listing:
         self.numListings = float(numListings)
         self.numDays = float(numDays)
 
-        self.bronx = 1 if borough == "Bronx" else 0
-        self.manhattan = 1 if borough == "Manhattan" else 0
-        self.queens = 1 if borough == "Queens" else 0
-        self.brooklyn = 1 if borough == "Brooklyn" else 0
-        self.staten = 1 if borough == "Staten Island" else 0
+        self.borough = make1HotVector(boroughLabels, borough)
 
-        self.shared = 1 if roomType == "Shared room" else 0
-        self.private = 1 if roomType == "Private room" else 0
-        self.entire = 1 if roomType == "Entire home/apt" else 0
+        self.roomType = make1HotVector(roomLabels, roomType)
+
+        self.neighborhood = make1HotVector(neighborhoodLabels, neighborhood)
         
         if latestReview == '':
             self.latestReview =     datetime.timedelta(-1)
@@ -41,43 +46,61 @@ data_reader=csv.reader(data, delimiter=',')
 
 totalData = []
 
+roomLabels = set()
+boroughLabels = set()
+neighborhoodLabels = set()
+
+for row in data_reader:
+    roomLabels.add(row[8])
+    boroughLabels.add(row[4])
+    neighborhoodLabels.add(row[5])
+data.seek(0)
+
 for row in data_reader:
     if row[0] == "id":  
         continue
-    totalData.append(Listing(row[4], row[8], row[11], row[12], row[13], row[14], row[15], row[9]))
+    totalData.append(Listing(row[4], row[5], row[8], row[11], row[12], row[13], row[14], row[15], row[9]))
 print(len(totalData))
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class Net(nn.Module):
-    def __init__(self):
+    def __init__(self, input_size):
         super(Net, self).__init__()
-        self.lin1 = nn.Linear(13, 7)
-        self.lin2 = nn.Linear(7, 1)
+        self.lin1 = nn.Linear(input_size, 1000)
+        self.lin2 = nn.Linear(1000, 500)
+        self.lin3 = nn.Linear(500, 250)
+        self.lin4 = nn.Linear(250, 1)
 
     def forward(self, x):
         x = F.relu(self.lin1(x))
-        x = self.lin2(x)
+        x = F.relu(self.lin2(x))
+        x = F.relu(self.lin3(x))
+        x = self.lin4(x)
         return x
 
-net = Net()
+
+
+random.shuffle(totalData)
+
+trainData = totalData[:34226]
+trainParamTensor = torch.tensor([[[i.reviews, i.reviewsPerMonth, i.numListings, i.numDays, i.latestReview.days] + i.borough + i.roomType + i.neighborhood] for i in trainData], dtype=torch.double).to(device).float()
+trainLabelTensor = torch.tensor([[[i.price]] for i in trainData], dtype=torch.double).to(device).float()
+
+validationData = totalData[34226:41561]
+validationParamTensor = torch.tensor([[[i.reviews, i.reviewsPerMonth, i.numListings, i.numDays, i.latestReview.days] + i.borough + i.roomType + i.neighborhood] for i in validationData], dtype=torch.double).to(device).float()
+validationLabelTensor = torch.tensor([[[i.price]] for i in validationData], dtype=torch.double).to(device).float()
+
+testData = totalData[41561:]
+testParamTensor = torch.tensor([[[i.reviews, i.reviewsPerMonth, i.numListings, i.numDays, i.latestReview.days] + i.borough + i.roomType + i.neighborhood] for i in testData], dtype=torch.double).to(device).float()
+testLabelTensor = torch.tensor([[[i.price]] for i in testData], dtype=torch.double).to(device).float()
+
+net = Net(trainParamTensor.shape[2])
 net.to(device)
 learningRate = 0.00001
 
 optimizer = optim.SGD(net.parameters(), lr=learningRate)
 
-random.shuffle(totalData)
-
-trainData = totalData[:34226]
-trainParamTensor = torch.tensor([[[i.reviews, i.reviewsPerMonth, i.numListings, i.numDays, i.bronx, i.manhattan, i.queens, i.brooklyn, i.staten, i.shared, i.private, i.entire, i.latestReview.days]] for i in trainData], dtype=torch.double).to(device).float()
-trainLabelTensor = torch.tensor([[[i.price]] for i in trainData], dtype=torch.double).to(device).float()
-
-validationData = totalData[34226:41561]
-validationParamTensor = torch.tensor([[[i.reviews, i.reviewsPerMonth, i.numListings, i.numDays, i.bronx, i.manhattan, i.queens, i.brooklyn, i.staten, i.shared, i.private, i.entire, i.latestReview.days]] for i in validationData], dtype=torch.double).to(device).float()
-validationLabelTensor = torch.tensor([[[i.price]] for i in validationData], dtype=torch.double).to(device).float()
-
-testData = totalData[41561:]
-testParamTensor = torch.tensor([[[i.reviews, i.reviewsPerMonth, i.numListings, i.numDays, i.bronx, i.manhattan, i.queens, i.brooklyn, i.staten, i.shared, i.private, i.entire, i.latestReview.days]] for i in testData], dtype=torch.double).to(device).float()
-testLabelTensor = torch.tensor([[[i.price]] for i in testData], dtype=torch.double).to(device).float()
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer)
 
 lossArray = []
 
@@ -94,11 +117,7 @@ while True:
         loss = lossfn(out, validationLabelTensor)
         print("Val loss " + str(loss.item()))
         lossArray.append(loss.item())
-
-    if (len(lossArray) > 10):
-        if lossArray[len(lossArray) - 1]> lossArray[len(lossArray) - 11]:
-            print("Done")
-            break
+        scheduler.step(loss)
 
 
 with torch.no_grad():
